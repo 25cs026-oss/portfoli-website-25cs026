c705c2682a75ac7e0e84828511b09030ac098e7c
KirtanK <25cs026@charusat.edu.in>
2025-11-21 23:47:05 +0530

    Update Projects.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -9,7 +9,7 @@ const Projects = () => {
     {
       title: "Chess Board GUI using C",
       description: "A C program that creates a graphical chessboard interface using basic graphics to visually represent an 8x8 chess grid with alternating colored squares, demonstrating fundamental GUI programming concepts in C on VS Code.",
-      image: Image1,
+      image: "https://github.com/25cs026-oss/portfoli-website-25cs026/blob/fafb465f7c2d827ceac8e8b5e48fb035dfef0e50/src/components/IMG-20251119-WA0001.jpg",
       technologies: ["C", "VS-Coder"],
       githubUrl: "https://github.com/25cs026-oss/C---Project-",
       liveUrl: "------------------------------",
@@ -17,7 +17,7 @@ const Projects = () => {
     {
       title: "Fire Asisstant System using ESP32",
       description: "A smart fire assistant system using ESP32, DHT11, MQ-2, and flame sensor to detect smoke, temperature, and flames for real-time safety monitoring and alerts.",
-      image: Image2,
+      image: "https://github.com/25cs026-oss/portfoli-website-25cs026/blob/fafb465f7c2d827ceac8e8b5e48fb035dfef0e50/src/components/IMG-20251121-WA0015.jpg",
       technologies: ["ESP32", "DHT11", "MQ-2","Flame Sensor"],
       githubUrl: "---Coming Soooonnnnn.........-----",
       liveUrl: "--------------------------------------",
